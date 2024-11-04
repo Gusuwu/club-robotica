d300888526b2cf80bb2cdecd6ce26de836e3265d
Gusuwu <augustoadrian@hotmail.com>
2024-11-04 19:12:35 -0300

    Demo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        violetaClub: '#9205cb',
+        violetaFondo: '#5f0683',
+      },
+      fontFamily: {
+        slab: ['"Alfa Slab One"', 'serif'], // Asegúrate de usar comillas dobles para nombres de fuentes con espacios
+      },
+    },
+  },
+  plugins: [],
+};
+
+
